ff873a85b706d56d54dcc7e093d62f4a4e093cbc
munir <smsirajulmonir@gmail.com>
2024-11-24 14:47:05 +0600

    car model implemented

---
+++ src/app/modules/car.model.ts
@@ -0,0 +1,22 @@
+import { model, Schema } from 'mongoose';
+import { Car } from './car/car.interface';
+
+
+const carSchema = new Schema<Car>({
+    brand: { type: String, required:true},
+    model: { type: String, required:true },
+    year: { type: Number, required:true, min:1994 },
+    price: { type: Number, required:true, min:0 },
+    category:{
+        type: String,
+        enum:["Sedan", "SUV", "Truck", "Coupe", "Convertible"],
+        required: true
+    },
+    description:{ type: String, required: true },
+    quantity:{ type: Number, required:true, min:0 },
+    inStock: { type: Boolean, required:true, default: true }
+},
+{ timestamps: true } 
+)
+
+const CarModel= model<Car>('CarModel',carSchema);
